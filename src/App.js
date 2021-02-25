22d029ba0aa91078fd6513c0cf54ccebee94f750
Mohammad Zakaria <zakaria.eee.iiuc@gmail.com>
2021-02-25 06:52:19 +0600

    added simple state on onClick event handler

--- src/App.js
+++ src/App.js
@@ -1,57 +1,76 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const students = ['Aafia', 'Yahya', 'Jahed', 'Zunaid', 'Zakariyya']
   const products = [
-    {name:'Photoshop', price:'$200'},
-    {name:'Ilustrator', price:'$400'},
-    {name:'Indesgn', price:'$333'},
-    {name:'PDF Reader', price:'$999'}
+    { name: 'Photoshop', price: '$200' },
+    { name: 'Illustrator', price: '$400' },
+    { name: 'Indesign', price: '$333' },
+    { name: 'PDF Reader', price: '$999' }
   ]
   const productNames = products.map(product => product.name);
   console.log(productNames);
   return (
     <div className="App">
       <header className="App-header">
+        <Counter></Counter>
         <p>I am a react Person!</p>
-        <Person name = {students[0]}></Person>
-        <Person name = {students[2]}></Person>
-        <Person name = {students[3]}></Person>
-        <Person name = {students[4]}></Person>
-        <Product product={products[0]} ></Product>
-        
+        <Person name={students}></Person>
+        {
+          products.map(pd => <Product product={pd}></Product>)
+        }
+
       </header>
     </div>
   );
 }
 
-function Product(props){
-  const productStyle = {
-    border: '1px solid gray',
-    borderRadius:'5px',
-    backgroundColor:'lightgrey',
-    height:'200px',
-    width: '200px',
-    float:'left'
-  }
-  
-  const {name, price} = props.product
-  console.log(props.product)
-  return (
-    <div style={productStyle}>
-      <h2>{name}</h2>
-      <h1>{price}</h1>
-      <button>Buy now</button>
+function Counter(){
+  const [count, setCount] = useState(10);
+  const handleIncrease = () => {
+    // const newCount = count + 1;
+    setCount(count + 1);
+  };
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count -1)}>Decrease</button>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
-  );
+  )
 }
 
-function Person(props){
+function Product(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgrey',
+    height: '300px',
+    width: '200px',
+    float: 'left'
+  }
+
+  const { name, price } = props.product
+  console.log(props.product)
+  return (
+        <div style={productStyle}>
+          <h2>{name}</h2>
+          <h1>{price}</h1>
+          <button>Buy now</button>
+        </div>
+        );
+
+}
+
+function Person(props) {
   return (
     <div>
       <ul>
-          <li>{props.name}</li>
+        {
+          props.name.map(student => <li>{student}</li>)
+        }
       </ul>
     </div>
   );
